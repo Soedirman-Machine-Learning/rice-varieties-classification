50d2ab1097b485bb4e01d3acdbde637045309f4e
Vidi005 <55016463+Vidi005@users.noreply.github.com>
2019-12-31 20:54:42 +0700

    Delete Deteksi.kt

--- Aplikasi Android/MobileNetV1/Rice-Variety-Classification/app/src/main/java/com/tflite/ricevarietyclassification/Deteksi.kt
+++
@@ -1,9 +0,0 @@
-package com.tflite.klasifikasivarietasberas
-
-data class Deteksi(
-    val name: String,
-    val probability: Float
-) {
-    override fun toString() =
-        "$name : ${probability*100}%"
-}
