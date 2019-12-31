7f054e4f17bd827f1039440548d1fa308db10802
Vidi005 <55016463+Vidi005@users.noreply.github.com>
2019-12-31 20:40:37 +0700

    Add files via upload

---
+++ Aplikasi Android/MobileNetV1/Rice-Varieties-Classification/app/src/main/java/com/tflite/ricevarietyclassification/Bantuan.kt
@@ -0,0 +1,17 @@
+package com.tflite.ricevarietyclassification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class Bantuan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bantuan)
+
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).title = "Halaman Bantuan"
+        }
+    }
+}
